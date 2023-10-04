4e76e2508c1cc793bdeff5aa5e06e3190cab6f3f
Johan0220 <nguyenhuy0220hihi@gmail.com>
2023-10-04 23:45:25 +0700

    Exam MVC

---
+++ T2207A_MVC/Entities/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using T2207A_MVC.Entities;
+using T2207A_MVC.Models;
+
+namespace T2207A_MVC.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Department> departments { get; set; }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
+
